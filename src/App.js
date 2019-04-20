99dc4561077d2c19dab93d0f493fb848a8073fc8
Stephen Wang <paradoxstephen@gmail.com>
2019-04-20 21:27:54 +1000

    passing component props

---
+++ src/App.js
@@ -0,0 +1,29 @@
+const Pet = (props) => {
+    return React.createElement('div', {}, [
+        React.createElement("h1", {}, props.name),
+        React.createElement("h2", {}, props.animal),
+        React.createElement("h2", {}, props.breed),
+    ]);
+}
+
+const App = () => {
+    return React.createElement("div", {}, [
+        React.createElement('h1', {}, 'Adopt Me!'),
+        React.createElement(Pet, {
+            name: "Luna",
+            animal: "dog",
+            breed: "Havaneses"
+        }),
+        React.createElement(Pet, {
+            name: "Pepper",
+            animal: "bird",
+            breed: "Cocktiel"
+        }),
+        React.createElement(Pet, {
+            name: "Doink",
+            animal: "cat",
+            breed: "Mixed"
+        }),
+    ])
+}
+ReactDOM.render(React.createElement(App), document.getElementById('root'));
