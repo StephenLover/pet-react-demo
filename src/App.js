072a79bb59b55f31be1fee23860d67542a6a2012
Stephen Wang <paradoxstephen@gmail.com>
2019-04-21 20:11:46 +1000

    Routing Finished

--- src/App.js
+++ src/App.js
@@ -1,68 +1,26 @@
 import React, { Component } from "react";
 import ReactDOM from "react-dom";
-import pf from "petfinder-client";
-import Pet from "./Pet";
-
-const petfinder = pf({
-  key: process.env.API_KEY,
-  secret: process.env.API_SECRET
-});
+import { Router, Link } from "@reach/router";
+import Results from "./Results";
+import Details from "./Details";
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      pets: []
-    };
-  }
-
-  componentDidMount() {
-    petfinder.pet
-      .find({ output: "full", location: "Seattle, WA" })
-      .then(data => {
-        let pets;
-        if (data.petfinder.pets && data.petfinder.pets.pet) {
-          if (Array.isArray(data.petfinder.pets.pet)) {
-            pets = data.petfinder.pets.pet;
-          } else {
-            pets = [data.petfinder.pets.pet];
-          }
-        } else {
-          pets = [];
-        }
-        this.setState({
-          pets: pets
-        });
-      });
-  }
-
   render() {
     return (
       <div>
-        <h1>Adopt Me!</h1>
-        <div>
-          {this.state.pets.map(pet => {
-            let breed;
-            if (Array.isArray(pet.breeds.breed)) {
-              breed = pet.breeds.breed.join(", ");
-            } else {
-              breed = pet.breeds.bread;
-            }
+        <header>
+          <Link to="/">
+            <h4>Adopt Me!</h4>
+          </Link>
+        </header>
 
-            return (
-              <Pet
-                key={pet.id}
-                animal={pet.animal}
-                name={pet.name}
-                breed={breed}
-                media={pet.media}
-                location={`${pet.contact.city}, ${pet.contact.state}`}
-              />
-            );
-          })}
-        </div>
+        <Router>
+          <Results path="/" />
+          <Details path="/details/:id" />
+        </Router>
       </div>
     );
   }
 }
+
 ReactDOM.render(<App />, document.getElementById("root"));
